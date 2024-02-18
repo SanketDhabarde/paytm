fa4d6278ac06dc9227093b6391cf1ec7554ce4ea
SanketDhabarde <sanketdhabarde13@gmail.com>
2024-02-19 00:04:13 +0530

    [Refactor]: Add restriction in user schema

--- backend/schema/user.schema.js
+++ backend/schema/user.schema.js
@@ -1,10 +1,31 @@
 const { model, Schema } = require("mongoose");
 
 const userSchema = new Schema({
-  username: String,
-  firstName: String,
-  lastName: String,
-  password: String,
+  username: {
+    type: String,
+    trim: true,
+    required: true,
+    unique: true,
+    minLength: 3,
+    maxLength: 30,
+  },
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+    maxLength: 50,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+    maxLength: 50,
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 6,
+  },
 });
 
 const User = model("User", userSchema);
