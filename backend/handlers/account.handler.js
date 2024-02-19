587a93aff3c01cd2593a7492ff05628998579a72
SanketDhabarde <sanketdhabarde13@gmail.com>
2024-02-20 00:00:36 +0530

    [Feat]: Add tranfer route with transections

--- backend/handlers/account.handler.js
+++ backend/handlers/account.handler.js
@@ -1,4 +1,5 @@
 const { Account } = require("../schema/account.schema");
+const mongoose = require("mongoose");
 
 const getUserBalance = async (req, res) => {
   const userId = req.userId;
@@ -7,7 +8,6 @@ const getUserBalance = async (req, res) => {
     if (!userAcc) {
       return res.status(411).json({ error: "User does not exists" });
     }
-    console.log(userAcc);
     return res.json({ balance: userAcc.balance });
   } catch (error) {
     console.log(error);
@@ -15,4 +15,46 @@ const getUserBalance = async (req, res) => {
   }
 };
 
-module.exports = { getUserBalance };
+const transferMoney = async (req, res) => {
+  const session = await mongoose.startSession();
+
+  await session.startTransaction();
+  const userId = req.userId;
+  const { to, amount } = req.body;
+  try {
+    const userAcc = await Account.findOne({ userId }).session(session);
+    if (!userAcc) {
+      return res.status(411).json({ error: "User does not exists" });
+    }
+
+    if (userAcc && userAcc.balance < amount) {
+      return res.status(400).json({ error: "Insufficient balance" });
+    }
+
+    const sender = await Account.findOne({ userId: to }).session(session);
+
+    if (!sender) {
+      return res.status(400).json({ error: "Invalid account" });
+    }
+
+    await Account.findOneAndUpdate(
+      { userId },
+      { $inc: { balance: -amount } }
+    ).session(session);
+    await Account.findOneAndUpdate(
+      { userId: sender.userId },
+      { $inc: { balance: amount } }
+    ).session(session);
+
+    await session.commitTransaction();
+    return res.json({ message: "Transfer successful" });
+  } catch (error) {
+    await session.abortTransaction();
+    console.log(error);
+    return res.status(500).json({ error: "Something went wrong" });
+  } finally {
+    await session.endSession();
+  }
+};
+
+module.exports = { getUserBalance, transferMoney };
--- backend/routes/account.route.js
+++ backend/routes/account.route.js
@@ -1,7 +1,11 @@
 const express = require("express");
-const { getUserBalance } = require("../handlers/account.handler");
+const {
+  getUserBalance,
+  transferMoney,
+} = require("../handlers/account.handler");
 const router = express.Router();
 
 router.route("/balance").get(getUserBalance);
+router.route("/transfer").post(transferMoney);
 
 module.exports = router;
