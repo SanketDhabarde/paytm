65bd739bb7b6bab4c792600c91c6932c721924e9
SanketDhabarde <sanketdhabarde13@gmail.com>
2024-02-20 20:53:54 +0530

    [Feat]: Initialize project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Signup from "./components/Signup";
+import Signin from "./components/Signin";
+import Dashboard from "./components/Dashboard";
+import SendMoney from "./components/SendMoney";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/signin" element={<Signin />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/send" element={<SendMoney />} />
+    </Routes>
+  );
+}
+
+export default App;
