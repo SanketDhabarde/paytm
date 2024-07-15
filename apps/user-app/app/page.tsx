90c4f79d5b1c42b13cf47cfc1eab7f6e2980b970
SanketDhabarde <sanketdhabarde13@gmail.com>
2024-07-15 11:09:44 +0530

    feat: add recoil

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,9 +1,11 @@
-
+"use client";
+import { useBalance } from "@repo/store/useBalance";
 
 export default function Home() {
+  const balance = useBalance();
   return (
     <h1 className="text-3xl font-bold underline">
-      Hello world!
+      Hello world with balance {balance}
     </h1>
   );
 }
